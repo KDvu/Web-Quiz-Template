e6f281300d3827515dbc2ee31a78125844eaa461
Kenny Vu <Kenny Vu>
2016-06-23 15:53:12 +0100

    Added validation for checkboxes

--- js/validation.js
+++ js/validation.js
@@ -5,5 +5,15 @@ function check(){
 	for(i=0;i<len;i++){
 		if(answer[i].checked)
 			alert("You chose " + answer[i].value);
-	}
+	}	
+	
+	var answer2 = document.getElementsByName("checkbox");
+	var len2 = answer2.length;
+	
+	for(i=0;i<len2;i++){
+		if(answer2[i].checked)
+			alert("You chose " + answer2[i].value);
+	}	
 }
+
+
