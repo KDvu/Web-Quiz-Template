0a55dc993f7f5af73521c5a4c1768a2934b18b52
Kenny Vu <Kenny Vu>
2016-06-23 17:11:28 +0100

    Added checkbox validation with limits i.e. the user can only select two boxes

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,20 @@
+function checkboxlimit(){
+	var checkgroup = document.getElementsByName("checkbox2");
+	var limit = 2;
+	
+	for (var i=0; i<checkgroup.length; i++){
+		checkgroup[i].onclick=function(){
+		var checkedcount=0
+		for (var i=0; i<checkgroup.length; i++)
+			checkedcount+=(checkgroup[i].checked)? 1 : 0
+		if (checkedcount>limit){
+			alert("You can only select a maximum of "+limit+" checkboxes")
+			this.checked=false
+			}
+		}
+	}
+}
+
 function check(){
 	var answer = document.getElementsByName("radio");
 	var len = answer.length;
@@ -15,5 +32,3 @@ function check(){
 			alert("You chose " + answer2[i].value);
 	}	
 }
-
-
