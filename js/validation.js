82755fe626dc898d01d4122db3ea07dd2ee86885
Kenny Vu <Kenny Vu>
2016-06-24 17:11:14 +0100

    Created two separate functions to check radio buttons and checkboxes

--- js/validation.js
+++ js/validation.js
@@ -1,30 +1,20 @@
-function checkboxlimit(){
-	var checkgroup = document.getElementsByName("checkbox2");
-	var limit = 2;
-	
-	for (var i=0; i<checkgroup.length; i++){
-		checkgroup[i].onclick=function(){
-		var checkedcount=0
-		for (var i=0; i<checkgroup.length; i++)
-			checkedcount+=(checkgroup[i].checked)? 1 : 0
-		if (checkedcount>limit){
-			alert("You can only select a maximum of "+limit+" checkboxes")
-			this.checked=false
-			}
-		}
-	}
+function check(){
+validateRadio(document.getElementsByName("radio"));
+validateCheckbox(document.getElementsByName("checkbox"));
 }
 
-function check(){
-	var answer = document.getElementsByName("radio");
+function validateRadio(key){
+	var answer = key;
 	var len = answer.length;
 	
 	for(i=0;i<len;i++){
 		if(answer[i].checked)
 			alert("You chose " + answer[i].value);
 	}	
-	
-	var answer2 = document.getElementsByName("checkbox");
+}
+
+function validateCheckbox(checkgroup){
+	var answer2 = checkgroup;
 	var len2 = answer2.length;
 	
 	for(i=0;i<len2;i++){
@@ -32,3 +22,25 @@ function check(){
 			alert("You chose " + answer2[i].value);
 	}	
 }
+
+function validateCheckboxLimit(checkgroup,limit){
+	//var checkgroup = document.getElementsByName("checkbox2");
+	//var limit = 2;
+	
+	var checkgroup = checkgroup;
+	var limit = limit;
+	
+	for (var i=0; i<checkgroup.length; i++){
+		checkgroup[i].onclick=function(){
+		var checkedcount=0;
+		
+		for (var i=0; i<checkgroup.length; i++)
+			checkedcount+=(checkgroup[i].checked)? 1 : 0
+		if (checkedcount>limit){
+			alert("You can only select a maximum of "+limit+" checkboxes");
+			this.checked=false;
+			}
+		}
+	}
+}
+
