a4a9a801c06e2072ddce6b5522d9fcd2a7d260c8
Kenny Vu <Kenny Vu>
2016-06-26 10:42:40 +0100

    Added disable hover effect when the submit button is clicked Also added explanatory comments to validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,10 +1,15 @@
 //This function is returned by onclick()
 function check(){
+	//Disables hover effect
+	$( ".answer" ).unbind(); 
+	
+	//Validate answers
 	validateRadio(document.getElementsByName("radio"));
 	validateCheckbox(document.getElementsByName("checkbox"));
 	validateCheckbox(document.getElementsByName("checkbox2"));
 }
 
+//Check given radio button
 function validateRadio(radio_button){
 	var answer = radio_button;
 	var len = answer.length;
@@ -21,6 +26,7 @@ function validateRadio(radio_button){
 	}	
 }
 
+//Check given checkbox
 function validateCheckbox(checkgroup){
 	var answer = checkgroup;
 	var len = answer.length;
@@ -38,6 +44,7 @@ function validateCheckbox(checkgroup){
 	checkAnswer(array,"checkbox");	
 }
 
+//Check if the limit on the number of boxes that can be checked is not exceeded
 function checkboxLimit(checkgroup,limit){
 	//var checkgroup = document.getElementsByName("checkbox2");
 	//var limit = 2;
@@ -59,6 +66,7 @@ function checkboxLimit(checkgroup,limit){
 	}
 }
 
+//Check the answer and prints the appropriate message
 function checkAnswer(answer,type){
 	if(type == "radio"){
 		alert("Checking radio");
