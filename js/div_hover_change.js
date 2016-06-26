366dc6afd4d43429105789da0020120124d7c21c
Kenny Vu <Kenny Vu>
2016-06-26 10:31:08 +0100

    Added hover effect for div tags around input types It changes the background color

---
+++ js/div_hover_change.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$(".answer").hover(function(){
+		$(this).css("background-color", "#E6E6FA");
+		}, function(){
+		$(this).css("background-color", "#F8F8FF");
+	});
+});
